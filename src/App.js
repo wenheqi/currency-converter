ba67cc2120d3c1b2b2d05d4ff304694d7d2d557a
wenheqi <wenhe.qi@outlook.com>
2020-05-14 12:32:48 -0700

    Print out currencies

--- src/App.js
+++ src/App.js
@@ -1,26 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Container, Row, Col } from "react-bootstrap";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import currencies from "./data/currencies.json";
+
+class App extends React.Component {
+  render() {
+    return (
+      <Container>
+        {currencies.map((currency) => {
+          return (
+            <Row>
+              <Col>
+                <img
+                  alt={currency.locale}
+                  src={
+                    "https://www.countryflags.io/" +
+                    currency.flag_name +
+                    "/shiny/64.png"
+                  }
+                ></img>
+              </Col>
+              <Col>{currency.locale}</Col>
+              <Col>{currency.currency_code}</Col>
+              <Col>{currency.currency_name}</Col>
+              <Col>{currency.currency_name_short}</Col>
+              <Col>{currency.display_unicode}</Col>
+            </Row>
+          );
+        })}
+      </Container>
+    );
+  }
 }
 
 export default App;
