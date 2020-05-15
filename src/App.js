88dd9e08f651a6f5cc8f1ec67456008292cee224
wenheqi <wenhe.qi@outlook.com>
2020-05-15 13:53:22 -0700

    Show responsive chart with static data

--- src/App.js
+++ src/App.js
@@ -1,35 +1,49 @@
 import React from "react";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { Container, Row, Col } from "react-bootstrap";
+import { Container, Row, Col, OverlayTrigger, Tooltip } from "react-bootstrap";
+
+import swapSvg from "./images/converter.svg";
 
 import currencies from "./data/currencies.json";
+
+import CurrencyCard from "./components/CurrencyCard";
+import HistorialCard from "./components/HistorialCard";
+
+// let HistoricalCardStyle = {
+//   min-height: 100vh!important;
+// }
 
 class App extends React.Component {
   render() {
     return (
       <Container>
-        {currencies.map((currency) => {
-          return (
-            <Row>
-              <Col>
-                <img
-                  alt={currency.locale}
-                  src={
-                    "https://www.countryflags.io/" +
-                    currency.flag_name +
-                    "/shiny/64.png"
-                  }
-                ></img>
-              </Col>
-              <Col>{currency.locale}</Col>
-              <Col>{currency.currency_code}</Col>
-              <Col>{currency.currency_name}</Col>
-              <Col>{currency.currency_name_short}</Col>
-              <Col>{currency.display_unicode}</Col>
-            </Row>
-          );
-        })}
+        <Row className="mt-3">
+          <Col sm={12} lg={5}>
+            <CurrencyCard />
+          </Col>
+          <Col sm={12} lg={2} className="m-auto">
+            <OverlayTrigger
+              key="tooltip-key-swap"
+              placement="top"
+              overlay={<Tooltip id="tooltip-swap3">Swap currencies</Tooltip>}
+            >
+              <img
+                className="img-fluid d-block mx-auto p-sm-2 p-lg-0 w-25"
+                src={swapSvg}
+                alt="swap"
+              ></img>
+            </OverlayTrigger>
+          </Col>
+          <Col sm={12} lg={5}>
+            <CurrencyCard />
+          </Col>
+        </Row>
+        <Row className="mt-3">
+          <Col className="min-vh-100">
+            <HistorialCard />
+          </Col>
+        </Row>
       </Container>
     );
   }
---
+++ src/components/CurrencyCard.js
@@ -0,0 +1,46 @@
+import React, { Component } from "react";
+import { Card, Button, ListGroup, Dropdown, Row, Col } from "react-bootstrap";
+import "./CurrencyCard.css";
+
+class CurrencyCard extends Component {
+  render() {
+    return (
+      <Card className="h-100 shadow-sm rounded">
+        <Card.Header className="bg-white">
+          <select className="form-control col-xs-3">
+            <option>USD</option>
+            <option>AED</option>
+            <option>CNY</option>
+          </select>
+        </Card.Header>
+        <Card.Body>
+          <Card.Title>United States Dollars</Card.Title>
+          <Card.Subtitle>$</Card.Subtitle>
+          <Row>
+            <Col sm={3}>
+              <img
+                alt="flag of United States"
+                src="https://www.countryflags.io/us/shiny/64.png"
+              />
+            </Col>
+            <Col sm={9}>
+              <input
+                type="number"
+                className="h-100 w-100 text-right border-top-0 border-left-0 border-right-0 fs-xlg"
+              />
+            </Col>
+          </Row>
+          <Row className="mt-2">
+            <Col>
+              <span className="fs-sm">
+                Last update: 12:00:00 pm 05/15/2020 PDT
+              </span>
+            </Col>
+          </Row>
+        </Card.Body>
+      </Card>
+    );
+  }
+}
+
+export default CurrencyCard;
