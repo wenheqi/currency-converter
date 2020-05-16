0acdc22e88948318aaa6389f7e55af1b0e3bf998
wenheqi <wenhe.qi@outlook.com>
2020-05-15 22:00:37 -0700

    Fix warning in Chrome

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import React from "react";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { Container, Row, Col, OverlayTrigger, Tooltip } from "react-bootstrap";
+import { OverlayTrigger, Tooltip } from "react-bootstrap";
 
 import swapSvg from "./images/converter.svg";
-
-import currencies from "./data/currencies.json";
 
 import CurrencyCard from "./components/CurrencyCard";
 import HistorialCard from "./components/HistorialCard";
@@ -18,20 +16,20 @@ class App extends React.Component {
   render() {
     return (
       <div
-        class="container"
+        className="container"
         style={{
           backgroundImage:
             "linear-gradient(45deg, rgb(0, 159, 221) 0%, rgb(0, 148, 141) 100%)",
         }}
       >
-        <div class="row min-vh-100">
-          <div class="col mt-3">
-            <div class="d-flex flex-column h-100">
-              <div class="row">
-                <div class="col-12 col-lg-5">
-                  <CurrencyCard />
+        <div className="row min-vh-100">
+          <div className="col mt-3">
+            <div className="d-flex flex-column h-100">
+              <div className="row">
+                <div className="col-12 col-lg-5">
+                  <CurrencyCard code="USD" />
                 </div>
-                <div class="col-12 col-lg-2 m-auto">
+                <div className="col-12 col-lg-2 m-auto">
                   <OverlayTrigger
                     key="tooltip-key-swap"
                     placement="top"
@@ -46,12 +44,12 @@ class App extends React.Component {
                     ></img>
                   </OverlayTrigger>
                 </div>
-                <div class="col-12 col-lg-5">
-                  <CurrencyCard />
+                <div className="col-12 col-lg-5">
+                  <CurrencyCard code="USD" />
                 </div>
               </div>
-              <div class="row flex-grow-1">
-                <div class="col mt-3">
+              <div className="row flex-grow-1">
+                <div className="col mt-3">
                   <HistorialCard />
                 </div>
               </div>
--- src/components/CurrencyCard.js
+++ src/components/CurrencyCard.js
@@ -1,47 +1,74 @@
-import React, { Component } from "react";
-import { Card, Button, ListGroup, Dropdown, Row, Col } from "react-bootstrap";
+import React, { useState, useEffect } from "react";
+import { Card, Row, Col } from "react-bootstrap";
 import "./CurrencyCard.css";
 
-class CurrencyCard extends Component {
-  render() {
-    return (
-      <Card className="h-100 shadow-sm rounded">
-        <Card.Header className="bg-white">
-          <select className="form-control col-xs-3">
-            <option>USD</option>
-            <option>AED</option>
-            <option>CNY</option>
-          </select>
-        </Card.Header>
-        <Card.Body>
-          <Card.Title>United States Dollars</Card.Title>
-          <Card.Subtitle>$</Card.Subtitle>
-          <Row>
-            <Col sm={3}>
-              <img
-                alt="flag of United States"
-                src="https://www.countryflags.io/us/shiny/64.png"
-              />
-            </Col>
-            <Col sm={9}>
-              <input
-                type="number"
-                title="Please enter a number, e.g. 123.987."
-                className="h-100 w-100 text-right border-top-0 border-left-0 border-right-0 fs-xlg"
-              />
-            </Col>
-          </Row>
-          <Row className="mt-2">
-            <Col>
-              <span className="fs-sm">
-                Last update: 12:00:00 pm 05/15/2020 PDT
-              </span>
-            </Col>
-          </Row>
-        </Card.Body>
-      </Card>
-    );
+import currencies from "../data/currencies";
+
+function CurrencyCard({ code }) {
+  const [currenciesJson, setCurrenciesJson] = useState({});
+  const [exchangeCode, setExchangeCode] = useState(code);
+
+  useEffect(() => {
+    if (currenciesJson === {}) {
+      let tmpJson = {};
+      currencies.forEach((currency) => {
+        tmpJson[currency.currency_code] = currency;
+        console.log(tmpJson);
+      });
+      setCurrenciesJson(tmpJson);
+      console.log(currenciesJson);
+    }
+  }, [currenciesJson]);
+
+  function changeSelect(event) {
+    setExchangeCode(event.target.value);
   }
+
+  return (
+    <Card className="h-100 shadow-sm rounded">
+      <Card.Header className="bg-white">
+        <select
+          className="form-control col-xs-3"
+          onChange={changeSelect}
+          value={exchangeCode}
+        >
+          {currencies.map((currency) => {
+            return (
+              <option key={currency.currency_code}>
+                {currency.currency_code}
+              </option>
+            );
+          })}
+        </select>
+      </Card.Header>
+      <Card.Body>
+        <Card.Title>US dollar</Card.Title>
+        <Card.Subtitle>$</Card.Subtitle>
+        <Row>
+          <Col sm={3}>
+            <img
+              alt="flag of United States"
+              src={"https://www.countryflags.io/be/shiny/64.png"}
+            />
+          </Col>
+          <Col sm={9}>
+            <input
+              type="number"
+              title="Please enter a number, e.g. 123.987."
+              className="h-100 w-100 text-right border-top-0 border-left-0 border-right-0 fs-xlg"
+            />
+          </Col>
+        </Row>
+        <Row className="mt-2">
+          <Col>
+            <span className="fs-sm">
+              Last update: 12:00:00 pm 05/15/2020 PDT
+            </span>
+          </Col>
+        </Row>
+      </Card.Body>
+    </Card>
+  );
 }
 
 export default CurrencyCard;
