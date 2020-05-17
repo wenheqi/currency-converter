0e1d21a9e095cb07d9333f40216fda6be39615c9
wenheqi <wenhe.qi@outlook.com>
2020-05-16 21:58:50 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -10,9 +10,15 @@ import HistorialCard from "./components/HistorialCard";
 
 function App() {
   const [srcCode, setSrcCode] = useState("USD");
-  const [srcTargetAmount, setSrcTargetAmount] = useState(0);
   const [dstCode, setDstCode] = useState("HKD");
-  const [dstTargetAmount, setDstTargetAmount] = useState(0);
+
+  const sendSrcCodeToParent = (code) => {
+    setSrcCode(code);
+  };
+
+  const sendDstCodeToParent = (code) => {
+    setDstCode(code);
+  };
 
   return (
     <div
@@ -27,35 +33,17 @@ function App() {
           <div className="d-flex flex-column h-100">
             <div className="row">
               <div className="col-12 col-lg-5">
-                <CurrencyCard
-                  srcCode={srcCode}
-                  setSrcCode={setSrcCode}
-                  dstCode={dstCode}
-                  amount={dstTargetAmount}
-                />
+                <CurrencyCard srcCode={srcCode} dstCode={dstCode} />
               </div>
               <div className="col-12 col-lg-2 m-auto">
-                <OverlayTrigger
-                  key="tooltip-key-swap"
-                  placement="top"
-                  overlay={
-                    <Tooltip id="tooltip-swap3">Swap currencies</Tooltip>
-                  }
-                >
-                  <img
-                    className="img-fluid d-block mx-auto p-4 p-lg-0 w-25"
-                    src={swapSvg}
-                    alt="swap"
-                  ></img>
-                </OverlayTrigger>
+                <img
+                  className="img-fluid d-block mx-auto p-4 p-lg-0 w-25"
+                  src={swapSvg}
+                  alt="swap"
+                ></img>
               </div>
               <div className="col-12 col-lg-5">
-                <CurrencyCard
-                  srcCode={dstCode}
-                  setSrcCode={setDstCode}
-                  dstCode={srcCode}
-                  amount={srcTargetAmount}
-                />
+                <CurrencyCard srcCode={dstCode} dstCode={srcCode} />
               </div>
             </div>
             <div className="row flex-grow-1">
